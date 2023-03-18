cab37e52c8cc4944b62e42f27ab4b8abecb60b8e
Siddharth <siddharthsriramka@gmail.com>
2023-03-18 22:40:38 +0530

    initial commit

---
+++ src/main/kotlin/com/my/plugins/Routing.kt
@@ -0,0 +1,14 @@
+package com.my.plugins
+
+import com.my.routes.*
+import io.ktor.server.application.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        customerRouting()
+        listOrdersRoute()
+        getOrderRoute()
+        totalizeOrderRoute()
+    }
+}
